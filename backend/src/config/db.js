5650b8eb0a1fd31cbffda7cfd19c0ea24c684850
DilipWebPandit <dilip7065kumar@gmail.com>
2026-03-06 00:49:03 +0530

    commit monogdb host

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(`${process.env.MONGO_URI}/buynest`);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    // console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("Database connection failed:", error.message);
     process.exit(1);
