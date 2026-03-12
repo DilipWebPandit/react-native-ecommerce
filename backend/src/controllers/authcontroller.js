a4765bd82bf1c4a2b4d052129fa0f2bacd6afd14
DilipWebPandit <143173356+DilipWebPandit@users.noreply.github.com>
2026-03-12 06:56:17 +0530

    Uncomment isAdmin field in auth response

--- backend/src/controllers/authcontroller.js
+++ backend/src/controllers/authcontroller.js
@@ -50,7 +50,7 @@ export const loginUser = async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
-        // isAdmin: user.isAdmin,
+        isAdmin: user.isAdmin,
         token: generatToken(user._id, user.name, user.isAdmin),
       });
     } else {
