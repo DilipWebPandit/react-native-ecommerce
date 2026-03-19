1a3c37ce00b79ed8385aacc653faac9039b0754c
DilipWebPandit <143173356+DilipWebPandit@users.noreply.github.com>
2026-03-19 23:23:29 +0530

    Update userModel.js

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -35,6 +35,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    profileImage: {
+  type: String,
+  default: "",
+},
     addresses: [addressSchema],
     isDeleted: {
       type: Boolean,
